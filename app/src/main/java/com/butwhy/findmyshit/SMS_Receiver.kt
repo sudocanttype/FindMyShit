9e13a3e9949e9e9fca19d5074e27be9867ce0bcf
Oscar Lay <oscarlay789@gmail.com>
2022-09-04 18:03:23 -0500

    okayyyy sending is now working

--- app/src/main/java/com/butwhy/findmyshit/SMS_Receiver.kt
+++ app/src/main/java/com/butwhy/findmyshit/SMS_Receiver.kt
@@ -8,13 +8,27 @@ import android.util.Log
 
 
 class SMS_Receiver : BroadcastReceiver() {
+    private var key: String = "423kslf"
+        get() {
+            return field
+        }
+        set(name) {
+            field = name
+        }
+
     override fun onReceive(context: Context, intent: Intent) {
         if (intent.action.toString() == "android.provider.Telephony.SMS_RECEIVED") {
-            Log.d("SMS_Receiver", "SMS received")
             val pdus = (intent.extras?.get("pdus")) as Array<*>
-            for(kekw in pdus){
-
-            }
+            for (kekw in pdus) {
+                val sms = SmsMessage.createFromPdu(kekw as ByteArray?).messageBody
+                if ( sms.count{ it == '-' } != 1 ){
+                    return 
+                }
+                val (test, content) = sms.split("-")
+                if (test == key) {
+                    Log.d("SMS_Receiver", content)
+                }
+        }
 
             // Will do stuff with message here
         }
