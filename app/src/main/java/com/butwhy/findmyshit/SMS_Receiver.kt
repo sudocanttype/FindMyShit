0c2d92c100a5e5ada9b4974600f8f8626c075523
Oscar Lay <oscarlay789@gmail.com>
2022-09-04 21:42:10 -0500

    impl sms response

--- app/src/main/java/com/butwhy/findmyshit/SMS_Receiver.kt
+++ app/src/main/java/com/butwhy/findmyshit/SMS_Receiver.kt
@@ -17,21 +17,37 @@ class SMS_Receiver : BroadcastReceiver() {
         }
 
     override fun onReceive(context: Context, intent: Intent) {
+        //whenever anything gets broadcasted this catches it
         if (intent.action.toString() == "android.provider.Telephony.SMS_RECEIVED") {
             val pdus = (intent.extras?.get("pdus")) as Array<*>
+            //the important shit is in the extras and pdus
             for (kekw in pdus) {
                 val sms = SmsMessage.createFromPdu(kekw as ByteArray?).messageBody
+                //pull out that msg data
                 if ( sms.count{ it == '-' } != 1 ){
                     return 
                 }
                 val (test, content) = sms.split("-")
                 if (test == key) {
+                    content = content.lowercase()
                     Log.d("SMS_Receiver", content)
-                }
-        }
+                    //check what the message is and act accordingly
+                    if ( content == "play" || content == "alarm" || content == "sound" || content == "ring"){
+                        //play a tone/alarm for easy finding
 
-            // Will do stuff with message here
+                    } else if ( content == "location" || content == "locate" || content == "find" || content == "place"){
+                        //send location preferably high accuracy
+
+                    } else if (  content == "lost" || content == "lost mode" || content == "lostmode" || content == "fuck" ){
+                        //lost mode -> begin logging locations every 3 minutes, send location every time screen is on
+
+                    }
+                }
+            }
         }
     }
 }
 
+//todo:
+//play alarm, send location, lost mode -> begin logging locations every 3 minutes, send location every time screen is on
+//
